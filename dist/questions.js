639653b2c11f2ee06fed89bce66aff9ea8372bd6
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-10-21 08:36:10 -0500

    created index question function export, and questions list for creating employees, jobs, and departments

---
+++ dist/questions.js
@@ -0,0 +1,72 @@
+import { employeeData, jobData, departmentData } from "./index.js";
+export const homeList = [
+    'View all employees',
+    'Add employee',
+    'Update employee role',
+    'View all roles',
+    'Add role',
+    'View all departments',
+    `Add department`,
+    `Exit`,
+];
+export async function createEmployee() {
+    const jobs = await jobData();
+    const employees = await employeeData();
+    const employeeQuestions = [
+        {
+            type: `input`,
+            name: `firstName`,
+            message: "Enter employee first name",
+        },
+        {
+            type: `input`,
+            name: `lastName`,
+            message: "Enter employee last name",
+        },
+        {
+            type: `list`,
+            name: `job`,
+            message: "Select employee job title",
+            choices: jobs,
+        },
+        {
+            type: `list`,
+            name: `manager`,
+            message: "Please select the employees manager",
+            choices: [
+                ...employees,
+                { name: 'None', value: null }
+            ],
+        },
+    ];
+    return employeeQuestions;
+}
+export async function createJob() {
+    const departments = await departmentData();
+    const jobQuestions = [
+        {
+            type: `input`,
+            name: `jobTitle`,
+            message: "Enter job title",
+        },
+        {
+            type: `number`,
+            name: `jobSalary`,
+            message: "Please enter this roles salary",
+        },
+        {
+            type: `list`,
+            name: `jobDepartment`,
+            message: "please assign this jobs department",
+            choices: departments,
+        },
+    ];
+    return jobQuestions;
+}
+export const depQuestions = [
+    {
+        type: `input`,
+        name: `depName`,
+        message: "Please enter demartment name",
+    },
+];
